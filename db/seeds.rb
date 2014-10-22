43add1533ee1670b80d87a724452df7fb8d3d944
cocodro <combswc@gmail.com>
2014-10-21 22:53:06 -0400

    Updated topics views and nested posts in topics

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,22 @@ require 'faker'
   user.skip_confirmation!
   user.save!
 end
-users= User.all
+users = User.all
+
+# Create Topics
+15.times do
+  Topic.create!(
+    name:        Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph
+  )
+end
+topics = Topic.all
 
 # Create Posts
 50.times do
   Post.create!(
     user: users.sample,
+    topic: topics.sample,
     title: Faker::Lorem.sentence,
     body:  Faker::Lorem.paragraph
   )
@@ -29,12 +39,37 @@ posts = Post.all
   )
 end
 
-User.first.update_attributes!(
-  email: 'combswc@gmail.com',
+#Create an admin user
+admin = User.new(
+  name:     'Admin User',
+  email:    'admin@example.com',
   password: 'helloworld',
+  role:     'admin'
 )
+admin.skip_confirmation!
+admin.save!
+
+#Create a moderator
+moderator = User.new(
+  name:     'Moderator User',
+  email:    'moderator@example.com',
+  password: 'helloworld',
+  role:     'moderator'
+)
+moderator.skip_confirmation!
+moderator.save!
+
+#Create a member
+member = User.new(
+name:    'Member User',
+email:   'member@example.com',
+password:'helloworld'
+)
+member.skip_confirmation!
+member.save!
 
 puts "Seed finished"
 puts "#{User.count} users created"
 puts "#{Post.count} posts created"
+puts "#{Topic.count} topics created"
 puts "#{Comment.count} comments created"
