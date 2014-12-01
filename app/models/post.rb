a69b8a81d5a47a7b915dc9cefac85d524ff24eea
cocodro <combswc@gmail.com>
2014-11-30 21:27:27 -0500

    Rspec

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,8 +8,8 @@ class Post < ActiveRecord::Base
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
-  #validates :topic, presence: true
-  #validates :user, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
 
   mount_uploader :image, ImageUploader
 
@@ -30,10 +30,6 @@ class Post < ActiveRecord::Base
     new_rank = points + age
     update_attribute(:rank, new_rank)
   end
-
-  after_create :create_vote
-  
-  private
 
   def create_vote
     user.votes.create(value: 1, post: self)
